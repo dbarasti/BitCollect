efe39d6fed0d057097476558df0b73c79abfecbe
Davide Barasti <dbarasti@gmail.com>
2020-07-06 17:59:21 +0200

    added docker network

--- truffle-config.js
+++ truffle-config.js
@@ -48,6 +48,12 @@ module.exports = {
       network_id: "*", // Any network (default: none)
     },
 
+    docker: {
+      host: "ganache-cli",
+      port: 7545,
+      network_id: "*"
+    }
+
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
